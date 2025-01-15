3dfc1cfcd887ab290df5e5e312f2cdd74e73bd14
Ulises Ruiz <106052507+Supertepo@users.noreply.github.com>
2025-01-15 16:14:56 -0600

    Modificando para tener carrusel

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from "react";
+
+const Carousel: React.FC = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slides = [
+    {
+      href: "Leccion/SmartPhone1",
+      src: "img/Celu Inte.jpg.png",
+      alt: "Los Angeles",
+    },
+    {
+      href: "servicio.html",
+      src: "img/ComPC.png",
+      alt: "Chicago",
+    },
+    {
+      href: "/Leccion/SmartPhone1",
+      src: "img/EjerdCurs.png",
+      alt: "New York",
+    },
+  ];
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + slides.length) % slides.length
+    );
+  };
+
+  // Automatically change slide every 5 seconds
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  // Add keyboard navigation
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowLeft") {
+        prevSlide();
+      } else if (event.key === "ArrowRight") {
+        nextSlide();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <div className="container mx-auto mt-8">
+      <div className="relative w-full overflow-hidden">
+        {/* The slideshow */}
+        <div
+          className="flex transition-transform duration-300 ease-in-out"
+          style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+        >
+          {slides.map((slide, index) => (
+            <div key={index} className="min-w-full">
+              <a href={slide.href}>
+                <img
+                  src={slide.src}
+                  alt={slide.alt}
+                  className="w-full h-auto"
+                />
+              </a>
+            </div>
+          ))}
+        </div>
+
+        {/* Indicators */}
+        <div className="absolute bottom-0 left-0 right-0 flex justify-center space-x-2 pb-4">
+          {slides.map((_, index) => (
+            <button
+              key={index}
+              className={`w-3 h-3 rounded-full ${
+                currentSlide === index ? "bg-blue-500" : "bg-gray-300"
+              }`}
+              onClick={() => setCurrentSlide(index)}
+            ></button>
+          ))}
+        </div>
+
+        {/* Left and right controls */}
+        <button
+          className="absolute top-0 bottom-0 left-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none focus:outline-none"
+          onClick={prevSlide}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-gray-800"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
+        </button>
+        <button
+          className="absolute top-0 bottom-0 right-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none focus:outline-none"
+          onClick={nextSlide}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-gray-800"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/components/Nav1.jsx
+++ src/components/Nav1.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,19 +11,49 @@ const Navbar = () => {
     <nav className="fixed w-full z-10 bg-white/10 shadow-md">
       <div className="max-w-screen-xl mx-auto px-4 py-4 flex items-center justify-between">
         <a href="/" className="flex items-center space-x-3">
-          <img src="/Tecnologia-para-todos.svg" alt="Logo de Tecnologia para todos" className="h-20" />
+          <img
+            src="img/TECPT.png"
+            alt="Logo de Tecnologia para todos"
+            className="h-40"
+          />
         </a>
         <div className="md:hidden">
-          <button onClick={toggleMenu} className="text-gray-800 focus:outline-none">
-            <svg className="size-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} />
+          <button
+            onClick={toggleMenu}
+            className="text-gray-800 focus:outline-none"
+          >
+            <svg
+              className="size-16"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+              />
             </svg>
           </button>
         </div>
-        <div className={`${isOpen ? 'block' : 'hidden'} w-full md:flex md:items-center md:space-x-3 md:justify-end`}>
-          <a href="/" className="text-white bg-[#a9afe3] hover:bg-[#cea9e3] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-3xl px-4 py-2 text-center block md:inline">Inicio</a>
-          <a href="/Lecciones" className="text-white bg-[#a9afe3] hover:bg-[#cea9e3] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-3xl px-4 py-2 text-center block md:inline">Lecciones</a>
-          <a href="/Ejercicios" className="text-white bg-[#a9afe3] hover:bg-[#cea9e3] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-3xl px-4 py-2 text-center block md:inline">Ejercicios</a>
+        <div
+          className={`${
+            isOpen ? "block" : "hidden"
+          } w-full md:flex md:items-center md:space-x-3 md:justify-end`}
+        >
+          <a
+            href="/"
+            className="text-white bg-[#a9afe3] hover:bg-[#cea9e3] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-3xl px-4 py-2 text-center block md:inline"
+          >
+            Inicio
+          </a>
+          <a
+            href="/Ejercicios"
+            className="text-white bg-[#a9afe3] hover:bg-[#cea9e3] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-3xl px-4 py-2 text-center block md:inline"
+          >
+            Ejercicios
+          </a>
         </div>
       </div>
     </nav>
