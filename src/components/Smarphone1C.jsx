5949273d1a0a814bfa4572bab2a02d9ea0b69c6f
Ulises Ruiz <106052507+Supertepo@users.noreply.github.com>
2025-01-15 14:41:43 -0600

    Modificando cuestionario de smartphone 1

--- src/components/Smarphone1C.jsx
+++ src/components/Smarphone1C.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
-import Confetti from "react-confetti";
+import React, { useState } from 'react';
+import Confetti from 'react-confetti';
 
 const correctAnswers = {
   pregunta1: "3",
   pregunta2: "1",
   pregunta3: "2",
+  pregunta4: "a",
+  pregunta5: "b",
+  pregunta6: "a",
 };
 
 const Cuestionario = () => {
@@ -12,6 +15,9 @@ const Cuestionario = () => {
     pregunta1: "",
     pregunta2: "",
     pregunta3: "",
+    pregunta4: "",
+    pregunta5: "",
+    pregunta6: "",
   });
 
   const [results, setResults] = useState({});
@@ -44,116 +50,129 @@ const Cuestionario = () => {
     setResults(results);
     setAllCorrect(allCorrect);
     setValidated(true);
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   return (
-    <div className="mx-auto max-w-5xl text-center">
-      {allCorrect && <Confetti />}
-      <h1 className="md:text-5xl text-6xl font-bold tracking-tight text-justify text-black">
-        Cuestionario
-      </h1>
-      <br />
-      <h2 className="md:text-4xl text-5xl font-bold tracking-tight text-justify text-black">
-        De las siguientes imágenes contesta las preguntas
-      </h2>
-      <br />
+    <div className="flex flex-col justify-center items-center min-h-screen bg-teal-300">
       {validated && (
         <div>
-          {allCorrect ? (
-            <div className="text-5xl text-green-500">
-              ¡Felicitaciones! Todas las respuestas son correctas.
-            </div>
-          ) : (
-            <div className="text-5xl text-red-500">
-              Sigue aprendiendo, ¡puedes hacerlo mejor!
-            </div>
-          )}
+          {allCorrect && <Confetti />}
+          <div className="verification">
+            {allCorrect ? (
+              <div className="text-5xl text-green-500">
+                ¡Felicitaciones! Has seleccionado las opciónes correctas.
+              </div>
+            ) : (
+              <div className="text-5xl text-red-500">
+                Incorrecto, sigue intentándolo.
+              </div>
+            )}
+          </div>
         </div>
       )}
-      <br />
-      <div className="grid grid-rows-1 md:grid-cols-3 pb-10 text-center items-center max-w-3xl mx-auto">
+      <h1 className="text-center text-3xl text-gray-800">Cuestionario</h1>
+      <form id="cuestionario" onSubmit={handleSubmit} className="bg-white p-5 rounded-lg shadow-lg max-w-md w-11/12">
         <div>
-          <img
-            src="/wifi.png"
-            alt="icono de wifi"
-            className="p-10 md:p-7 rounded-full"
-          />
-          <p className="text-2xl">1</p>
-        </div>
-        <div>
-          <img
-            src="/Linterna.png"
-            alt="icono de linterna"
-            className="p-10 md:p-7 rounded-full"
-          />
-          <p className="text-2xl">2</p>
-        </div>
-        <div>
-          <img
-            src="/Rotacion.png"
-            alt="icono de rotación de pantalla"
-            className="p-10 md:p-7 rounded-full"
-          />
-          <p className="text-2xl">3</p>
-        </div>
-      </div>
-      <br />
-      <form
-        onSubmit={handleSubmit}
-        className="items-center grid grid-rows-3 max-w-xl mx-auto"
-      >
-        <div>
-          <label className="text-4xl text-center">
-            ¿Cuál es el icono para “Rotación automática de pantalla”?
+          <h3 className="text-lg font-bold text-gray-700">1. ¿Cuál es el icono para “Rotación automática de pantalla”?</h3>
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta1" value="1" onChange={handleChange} className="mr-2" />
+            <img src="/img/Wifi.png" alt="Icono 1" width="100" />
           </label>
-          <br />
-          <br />
-          <input
-            className="bg-gray-50 border w-full border-gray-300 text-gray-900 rounded-lg p-2.5 dark:border-gray-600 dark:placeholder-gray-400"
-            type="text"
-            name="pregunta1"
-            onChange={handleChange}
-          />
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta1" value="2" onChange={handleChange} className="mr-2" />
+            <img src="/img/Linter.png" alt="Icono 2" width="100" />
+          </label>
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta1" value="3" onChange={handleChange} className="mr-2" />
+            <img src="/img/RotateAp.png" alt="Icono 3" width="100" />
+          </label>
         </div>
+
         <div>
-          <label className="text-4xl">¿Cuál es el icono para “Wi-Fi”?</label>
-          <br />
-          <br />
-          <input
-            className="bg-gray-50 border w-full border-gray-300 text-gray-900 rounded-lg block p-2.5 dark:border-gray-600 dark:placeholder-gray-400"
-            type="text"
-            name="pregunta2"
-            onChange={handleChange}
-          />
+          <h3 className="text-lg font-bold text-gray-700">2. ¿Cuál es el icono para “Wi-Fi”?</h3>
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta2" value="1" onChange={handleChange} className="mr-2" />
+            <img src="/img/Wifi.png" alt="Icono 4" width="100" />
+          </label>
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta2" value="2" onChange={handleChange} className="mr-2" />
+            <img src="/img/Linter.png" alt="Icono 5" width="100" />
+          </label>
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta2" value="3" onChange={handleChange} className="mr-2" />
+            <img src="/img/RotateAp.png" alt="Icono 6" width="100" />
+          </label>
         </div>
+
         <div>
-          <label className="text-4xl">¿Cuál es el icono para “Linterna”?</label>
-          <br />
-          <br />
-          <input
-            className="bg-gray-50 border w-full border-gray-300 text-gray-900 rounded-lg block p-2.5 dark:border-gray-600 dark:placeholder-gray-400"
-            type="text"
-            name="pregunta3"
-            onChange={handleChange}
-          />
+          <h3 className="text-lg font-bold text-gray-700">3. ¿Cuál es el icono para “Linterna”?</h3>
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta3" value="1" onChange={handleChange} className="mr-2" />
+            <img src="/img/Wifi.png" alt="Icono 7" width="100" />
+          </label>
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta3" value="2" onChange={handleChange} className="mr-2" />
+            <img src="/img/Linter.png" alt="Icono 8" width="100" />
+          </label>
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta3" value="3" onChange={handleChange} className="mr-2" />
+            <img src="/img/RotateAp.png" alt="Icono 9" width="100" />
+          </label>
         </div>
-        <button
-          className="text-white bg-[#a9afe3] hover:bg-[#cea9e3] focus:ring-4 focus:outline-none focus:ring-blue-300 md:font-medium rounded-lg md:text-3xl text-5xl px-1 md:px-4 text-center"
-          type="submit"
-        >
-          Verificar Respuestas
-        </button>
+
+        <div>
+          <h3 className="text-lg font-bold text-gray-700">4. ¿Cuál es la siguiente función y para qué sirve?</h3>
+          <img src="/img/NdBrillo.png" alt="Función ilustrativa" className="w-full mb-2" />
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta4" value="a" onChange={handleChange} className="mr-2" />
+            Nivel de brillo: Su función es subir o bajar el brillo del celular a gusto propio.
+          </label><br />
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta4" value="b" onChange={handleChange} className="mr-2" />
+            Luz nocturna: Su función es darnos una luz cálida para ocuparse en la noche sin afectar la vista al usuario.
+          </label><br />
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta4" value="c" onChange={handleChange} className="mr-2" />
+            Ahorro de energía: Su función es no gastar tanta energía del celular, esto si no tenemos un centro de carga cercano.
+          </label>
+        </div>
+
+        <div>
+          <h3 className="text-lg font-bold text-gray-700">5. Para ver las Notificaciones (mensajes, llamadas o alertas) necesitamos deslizar nuestro dedo ____ hacia ____.</h3>
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta5" value="a" onChange={handleChange} className="mr-2" />
+            De abajo hacia arriba.
+          </label><br />
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta5" value="b" onChange={handleChange} className="mr-2" />
+            De arriba hacia abajo.
+          </label>
+        </div>
+
+        <div>
+          <h3 className="text-lg font-bold text-gray-700">6. Para ver Todas las aplicaciones del celular necesitamos deslizar nuestro dedo ____ hacia ____.</h3>
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta6" value="a" onChange={handleChange} className="mr-2" />
+            De abajo hacia arriba.
+          </label><br />
+          <label className="flex items-center mb-2">
+            <input type="radio" name="pregunta6" value="b" onChange={handleChange} className="mr-2" />
+            De arriba hacia abajo.
+          </label>
+        </div>
+
+        <button type="submit" className="block w-full py-2 bg-blue-500 text-white rounded-lg text-lg font-medium hover:bg-blue-700">Enviar</button>
       </form>
-      <h2>Resultados</h2>
-      {Object.keys(results).length > 0 && (
-        <ul>
+
+      {validated && (
+        <div id="resultado" className="text-center mt-5 text-lg font-bold">
           {Object.keys(results).map((key) => (
-            <li key={key}>
+            <p key={key}>
               {key}: {results[key] ? "Correcto" : "Incorrecto"}
-            </li>
+            </p>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
