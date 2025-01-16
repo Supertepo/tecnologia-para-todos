13ab73ee6d44baf4d9135c89206666cd2458feee
Ulises Ruiz <106052507+Supertepo@users.noreply.github.com>
2025-01-15 18:38:06 -0600

    Update: Diseño enfocado a visibilidad

--- src/components/Nav1.jsx
+++ src/components/Nav1.jsx
@@ -44,13 +44,13 @@ const Navbar = () => {
         >
           <a
             href="/"
-            className="text-white bg-[#a9afe3] hover:bg-[#cea9e3] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-3xl px-4 py-2 text-center block md:inline"
+            className="bg-[#a9afe3] hover:bg-[#cea9e3] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-3xl px-4 py-2 text-center block md:inline"
           >
             Inicio
           </a>
           <a
             href="/Ejercicios"
-            className="text-white bg-[#a9afe3] hover:bg-[#cea9e3] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-3xl px-4 py-2 text-center block md:inline"
+            className="bg-[#a9afe3] hover:bg-[#cea9e3] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-3xl px-4 py-2 text-center block md:inline"
           >
             Ejercicios
           </a>
--- src/components/Smarphone1C.jsx
+++ src/components/Smarphone1C.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import Confetti from 'react-confetti';
+import React, { useState } from "react";
+import Confetti from "react-confetti";
 
 const correctAnswers = {
   pregunta1: "3",
@@ -50,11 +50,11 @@ const Cuestionario = () => {
     setResults(results);
     setAllCorrect(allCorrect);
     setValidated(true);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center m-10">
       {validated && (
         <div>
           {allCorrect && <Confetti />}
@@ -71,98 +71,235 @@ const Cuestionario = () => {
           </div>
         </div>
       )}
-      <h1 className="text-center text-3xl text-gray-800">Cuestionario</h1>
-      <form id="cuestionario" onSubmit={handleSubmit} className=" p-5 rounded-lg shadow-xl ">
+      <h1 className="text-center text-6xl text-gray-800">Cuestionario</h1>
+      <form
+        id="cuestionario"
+        onSubmit={handleSubmit}
+        className=" p-5 rounded-lg shadow-xl "
+      >
         <div>
-          <h3 className="text-lg font-bold text-gray-700">1. ¿Cuál es el icono para “Rotación automática de pantalla”?</h3>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta1" value="1" onChange={handleChange} className="mr-2" />
-            <img src="/img/Wifi.png" alt="Icono 1" width="100" />
+          <h3 className="text-3xl font-bold text-gray-700">
+            1. ¿Cuál es el icono para “Rotación automática de pantalla”?
+          </h3>
+          <div className="grid md:grid-cols-4">
+            <label className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="pregunta1"
+                value="1"
+                onChange={handleChange}
+                className="mr-2 size-7"
+              />
+              <img src="/img/Wifi.png" alt="Icono 1" className="size-40" />
+            </label>
+            <label className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="pregunta1"
+                value="2"
+                onChange={handleChange}
+                className="mr-2 size-7"
+              />
+              <img src="/img/Linter.png" alt="Icono 2" className="size-40" />
+            </label>
+            <label className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="pregunta1"
+                value="3"
+                onChange={handleChange}
+                className="mr-2 size-7"
+              />
+              <img src="/img/RotateAp.png" alt="Icono 3" className="size-40" />
+            </label>
+          </div>
+        </div>
+
+        <div>
+          <h3 className="text-3xl font-bold text-gray-700">
+            2. ¿Cuál es el icono para “Wi-Fi”?
+          </h3>
+          <div className="grid md:grid-cols-4">
+            <label className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="pregunta2"
+                value="1"
+                onChange={handleChange}
+                className="mr-2 size-7"
+              />
+              <img src="/img/Wifi.png" alt="Icono 4" className="size-40" />
+            </label>
+            <label className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="pregunta2"
+                value="2"
+                onChange={handleChange}
+                className="mr-2 size-7"
+              />
+              <img src="/img/Linter.png" alt="Icono 5" className="size-40" />
+            </label>
+            <label className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="pregunta2"
+                value="3"
+                onChange={handleChange}
+                className="mr-2 size-7"
+              />
+              <img src="/img/RotateAp.png" alt="Icono 6" className="size-40" />
+            </label>
+          </div>
+        </div>
+
+        <div>
+          <h3 className="text-3xl font-bold text-gray-700">
+            3. ¿Cuál es el icono para “Linterna”?
+          </h3>
+          <div className="grid md:grid-cols-4">
+            <label className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="pregunta3"
+                value="1"
+                onChange={handleChange}
+                className="mr-2 size-7"
+              />
+              <img src="/img/Wifi.png" alt="Icono 7" className="size-40" />
+            </label>
+
+            <label className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="pregunta3"
+                value="2"
+                onChange={handleChange}
+                className="mr-2 size-7"
+              />
+              <img src="/img/Linter.png" alt="Icono 8" className="size-40" />
+            </label>
+            <label className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="pregunta3"
+                value="3"
+                onChange={handleChange}
+                className="mr-2 size-7"
+              />
+              <img src="/img/RotateAp.png" alt="Icono 9" className="size-40" />
+            </label>
+          </div>
+        </div>
+
+        <div>
+          <h3 className="text-3xl font-bold text-gray-700">
+            4. ¿Cuál es la siguiente función y para qué sirve?
+          </h3>
+          <img
+            src="/img/NdBrillo.png"
+            alt="Función ilustrativa"
+            className="w-auto mb-2"
+          />
+          <label className="flex items-center mb-2 text-2xl">
+            <input
+              type="radio"
+              name="pregunta4"
+              value="a"
+              onChange={handleChange}
+              className="mr-2 size-7"
+            />
+            Nivel de brillo: Su función es subir o bajar el brillo del celular a
+            gusto propio.
           </label>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta1" value="2" onChange={handleChange} className="mr-2" />
-            <img src="/img/Linter.png" alt="Icono 2" width="100" />
+          <br />
+          <label className="flex items-center mb-2 text-2xl">
+            <input
+              type="radio"
+              name="pregunta4"
+              value="b"
+              onChange={handleChange}
+              className="mr-2 size-7"
+            />
+            Luz nocturna: Su función es darnos una luz cálida para ocuparse en
+            la noche sin afectar la vista al usuario.
           </label>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta1" value="3" onChange={handleChange} className="mr-2" />
-            <img src="/img/RotateAp.png" alt="Icono 3" width="100" />
+          <br />
+          <label className="flex items-center mb-2 text-2xl">
+            <input
+              type="radio"
+              name="pregunta4"
+              value="c"
+              onChange={handleChange}
+              className="mr-2 size-7"
+            />
+            Ahorro de energía: Su función es no gastar tanta energía del
+            celular, esto si no tenemos un centro de carga cercano.
           </label>
         </div>
 
         <div>
-          <h3 className="text-lg font-bold text-gray-700">2. ¿Cuál es el icono para “Wi-Fi”?</h3>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta2" value="1" onChange={handleChange} className="mr-2" />
-            <img src="/img/Wifi.png" alt="Icono 4" width="100" />
-          </label>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta2" value="2" onChange={handleChange} className="mr-2" />
-            <img src="/img/Linter.png" alt="Icono 5" width="100" />
-          </label>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta2" value="3" onChange={handleChange} className="mr-2" />
-            <img src="/img/RotateAp.png" alt="Icono 6" width="100" />
-          </label>
-        </div>
-
-        <div>
-          <h3 className="text-lg font-bold text-gray-700">3. ¿Cuál es el icono para “Linterna”?</h3>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta3" value="1" onChange={handleChange} className="mr-2" />
-            <img src="/img/Wifi.png" alt="Icono 7" width="100" />
-          </label>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta3" value="2" onChange={handleChange} className="mr-2" />
-            <img src="/img/Linter.png" alt="Icono 8" width="100" />
-          </label>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta3" value="3" onChange={handleChange} className="mr-2" />
-            <img src="/img/RotateAp.png" alt="Icono 9" width="100" />
-          </label>
-        </div>
-
-        <div>
-          <h3 className="text-lg font-bold text-gray-700">4. ¿Cuál es la siguiente función y para qué sirve?</h3>
-          <img src="/img/NdBrillo.png" alt="Función ilustrativa" className="w-full mb-2" />
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta4" value="a" onChange={handleChange} className="mr-2" />
-            Nivel de brillo: Su función es subir o bajar el brillo del celular a gusto propio.
-          </label><br />
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta4" value="b" onChange={handleChange} className="mr-2" />
-            Luz nocturna: Su función es darnos una luz cálida para ocuparse en la noche sin afectar la vista al usuario.
-          </label><br />
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta4" value="c" onChange={handleChange} className="mr-2" />
-            Ahorro de energía: Su función es no gastar tanta energía del celular, esto si no tenemos un centro de carga cercano.
-          </label>
-        </div>
-
-        <div>
-          <h3 className="text-lg font-bold text-gray-700">5. Para ver las Notificaciones (mensajes, llamadas o alertas) necesitamos deslizar nuestro dedo ____ hacia ____.</h3>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta5" value="a" onChange={handleChange} className="mr-2" />
+          <h3 className="text-3xl font-bold text-gray-700">
+            5. Para ver las Notificaciones (mensajes, llamadas o alertas)
+            necesitamos deslizar nuestro dedo ____ hacia ____.
+          </h3>
+          <label className="flex items-center mb-2 text-2xl">
+            <input
+              type="radio"
+              name="pregunta5"
+              value="a"
+              onChange={handleChange}
+              className="mr-2 size-7"
+            />
             De abajo hacia arriba.
-          </label><br />
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta5" value="b" onChange={handleChange} className="mr-2" />
+          </label>
+          <br />
+          <label className="flex items-center mb-2 text-2xl">
+            <input
+              type="radio"
+              name="pregunta5"
+              value="b"
+              onChange={handleChange}
+              className="mr-2 size-7"
+            />
             De arriba hacia abajo.
           </label>
         </div>
 
         <div>
-          <h3 className="text-lg font-bold text-gray-700">6. Para ver Todas las aplicaciones del celular necesitamos deslizar nuestro dedo ____ hacia ____.</h3>
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta6" value="a" onChange={handleChange} className="mr-2" />
+          <h3 className="text-3xl font-bold text-gray-700">
+            6. Para ver Todas las aplicaciones del celular necesitamos deslizar
+            nuestro dedo ____ hacia ____.
+          </h3>
+          <label className="flex items-center mb-2 text-2xl">
+            <input
+              type="radio"
+              name="pregunta6"
+              value="a"
+              onChange={handleChange}
+              className="mr-2 size-7"
+            />
             De abajo hacia arriba.
-          </label><br />
-          <label className="flex items-center mb-2">
-            <input type="radio" name="pregunta6" value="b" onChange={handleChange} className="mr-2" />
+          </label>
+          <br />
+          <label className="flex items-center mb-2 text-2xl">
+            <input
+              type="radio"
+              name="pregunta6"
+              value="b"
+              onChange={handleChange}
+              className="mr-2 size-7"
+            />
             De arriba hacia abajo.
           </label>
         </div>
 
-        <button type="submit" className="block w-full py-2 bg-blue-500 text-white rounded-lg text-lg font-medium hover:bg-blue-700">Enviar</button>
+        <button
+          type="submit"
+          className="block w-full md:w-auto py-2 px-10 bg-blue-500 text-white rounded-lg text-3xl font-medium hover:bg-blue-700"
+        >
+          Enviar
+        </button>
       </form>
 
       {validated && (
